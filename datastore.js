59e03e7a18abc0f0407e86cff5a9057e61497e56
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-07-06 06:25:12 -0500

    questions run in order

---
+++ datastore.js
@@ -0,0 +1,52 @@
+const STORE = [
+	{
+		question: 'What is the first thing you should do when you change an apparatus',
+		answers: ['Untying the wraps, starting with the locking wraps.', 'Give the line you’re working with a tug to make sure you’re on the correct line.', 'Tell everyone what you’re doing.', 'Do the rig dance.'], 
+		correctAnswer: 'Give the line you’re working with a tug to make sure you’re on the correct line.'
+
+	},
+	{
+		question: 'For our pinrail system, how many regular wraps do you tie?',
+		answers: ['3', '2', '1', '4'],
+		correctAnswer: '2'
+	},
+
+	{
+		question: 'For our pinrail system, how many locking wraps do you tie?',
+		answers: ['3', '2', '1', 'all'],
+		correctAnswer: '2'
+	},
+
+	{
+		question: 'Why is it important that everyone tie off the pinrails with the same number and type of wraps?',
+		answers: ['It looks nicer.', 'If an aerial rescue needs to happen, the person doing the rescue will automatically know the number of wraps.', 'It doesn’t matter.', 'None of the above.'],
+		correctAnswer: 'If an aerial rescue needs to happen, the person doing the rescue will automatically know the number of wraps.'
+	},
+
+	{
+		question: 'What are common aerial rescue situations?',
+		answers: ['Aerialist getting tangled in the silks or stuck in the trapeze ropes.', 'Clothing getting caught.', 'Fear of heights.', 'All of the above.'],
+		correctAnswer: 'All of the above.'
+	},
+
+	{
+		question: 'Why do we tie two regular wraps on the pinrail (before the locking ones)?',
+		answers: ['It takes less time than doing 4 locking wraps.', 'It looks nicer.', 'It helps with aerial rescue.', 'None of the above.'],
+		correctAnswer: 'It helps with aerial rescue.'
+	},
+
+		{
+		question: 'What are the triangles used for in rigging?',
+		answers: ['Single point trapeze.', 'Double point lyra.', 'Both of the above.', 'None of the above.'],
+		correctAnswer: 'Both of the above.'
+	},
+
+	{
+		question: 'Which way should you rig a carabiner?',
+		answers: ['So that it “screws down” (top down).', 'So that it “screws up” (top up).', 'Either.', 'Both/either B &C.'],
+		correctAnswer: 'None of the above.'
+	},
+
+
+
+]
--- index.js
+++ index.js
@@ -1,44 +1,98 @@
-'use strict';
-
-let questionNumer=0;
-
-let score=0;
 
 
-function renderLandingPage () {
+let questionNumber=0;
+
+//let score=0;
+
+
+/*function renderLandingPage () {
 //this function will be resposible for rendering the landing page in the DOM
 
 	console.log('`renderLandingPage` ran');
+}*/
+
+
+function handleClickToStart (){
+
+	$('.quizStart').on('click', '.startButton', function(event) {
+		$('.quizStart').remove();
+
+		event.preventDefault();
+		renderQuestion();
+
+		console.log('Clicked start button!');
+		/*$('.questionForm').css('display', 'block');
+    	$('.questionNumber').text(1);*/
+
+		});
+	//this function will be responsible for when the user clicks the "I'm ready" button to start the quiz
+}
+
+
+//this function will be responsible for generating the question HTML
+function generateQuestion (){
+	if (questionNumber < STORE.length) {
+		return `<div class="question-${questionNumber}">
+    	<h2>${STORE[questionNumber].question}</h2>
+    	<form>
+    	<fieldset>
+    	<label class="answerOption">
+    	<input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+    	<span>${STORE[questionNumber].answers[0]}</span>
+    	</label>
+    	<label class="answerOption">
+    	<input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+    	<span>${STORE[questionNumber].answers[1]}</span>
+    	</label>
+    	<label class="answerOption">
+    	<input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+    	<span>${STORE[questionNumber].answers[2]}</span>
+    	</label>
+    	<label class="answerOption">
+    	<input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+    	<span>${STORE[questionNumber].answers[3]}</span>
+    	</label>
+    	<button type="submit" class="nextButton">Next</button>
+    	</fieldset>
+    	</form>
+    </div>`;
+}
+
+	/*else{
+	console.log('no generator!');
+}*/
+
+	console.log('generate question ran');
+}
+
+
+
+function renderQuestion(){
+
+	console.log('render question ran!');
+
+	$('.questionForm').html(generateQuestion());
+
 }
 
 function changeQuestionNumber(){
-	//increment the question number
+		questionNumber ++;
+
+	$('.questionNumber').text(questionNumber+1);
+	console.log(questionNumber);
+	
 }
 
-function changeScore(){
+/*function changeScore(){
 	//increment the user score
 }
 
-function handleClickToStart (){
-	//this function will be responsible for when the user clicks the "I'm ready" button to start the quiz
-
-	console.log('`handleClickToStart` ran');
-}
-
-function generateQuestion (){
-	//this function will be responsible for generating the question HTML
-	console.log('`generateQuestion` ran');
-}
-
-function renderQuestion(){
-	//render the question in the DOM
-}
-
 function selectAnswer(){
-	//user clicks on an answer 
+	
+
 }
 
-function upDateScoreText(){
+/*function upDateScoreText(){
 	//this function is responsible for updating the text of the score the users see
 }
 
@@ -59,29 +113,35 @@ function answerCorrectFeedback() {
 function answerIncorrectFeedback(){
 	//displays feedback the user sees if they answer incorrectly
 	ifAnswerIsIncorrect();
-}
+}*/
 
 function nextQuestion (){
-	//what happens when the user clicks "Next"
+	$('main').on('click','.nextButton', function(event) {
+
 	changeQuestionNumber();
+	console.log('next question click ran!')
 	renderQuestion();
-	selectAnswer();
+	});
+	//selectAnswer();
 }
 
-function renderResults(){
+/*function renderResults(){
 	//responsible for the HTML for when the quiz is over
 }
 
 function startQuizOver(){
 	//reloads the page to start quiz over
-}
+}*/
 
 function createQuiz () {
   handleClickToStart();
-  renderQuestion();
-  selectAnswer();
-  NextQuestion();
+  console.log('create quiz ran!')
+  nextQuestion();
+  //selectAnswer();
+ // NextQuestion();
 }
 
 $(createQuiz);
+
+//console.log (STORE[0]);
 
